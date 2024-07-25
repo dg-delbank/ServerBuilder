8ddb14c0350e72b1531fa253140e2086c8e5de40
dg-delbank <douglas.santos@delbank.com.br>
2024-07-25 11:33:54 -0300

    Changes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 print(' Default:\n-------1.Portainer\n-------2.Nginx Proxy Manager\n-------3.Jenkins')
 print('Para seleção manual digite o(s) valor(es) numério(s), separado(s) por vírgula(s).')
@@ -16,6 +17,11 @@ def verify_choice(choice):
         name = input('Digite o nome do container do Portainer: ') or 'portainer-delbank-hml-master'
         os.system(f'docker volume create portainer_data')
         os.system(f'docker run -d -p 8000:8000 -p 9443:9443 --name {name} --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest')
+
+        with open('ips.txt', 'w') as f:
+            ip = subprocess.check_output(f"docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' {name}", shell=True, text=True).strip()
+            f.write(f'{name}:{ip}')
+
     if '2' in choice:
         name = input('Digite o nome do container do Nginx: ') or 'nginx-proxy-manager-delbank-hml-master'
         with open('docker-compose.yml', 'w') as f:
@@ -35,9 +41,18 @@ services:
 ''')
         os.system('docker compose up -d')
 
+        with open('ips.txt', 'w') as f:
+            ip = subprocess.check_output(f"docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' {name}", shell=True, text=True).strip()
+            f.write(f'{name}:{ip}')
+
     if '3' in choice:
         name = input('Digite o nome do container do Jenkins: ') or 'jenkins-delbank-hml-master'
         os.system(f'docker run -d -it --restart always -u root -p 8080:8080 -p 50000:50000 -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker --name {name} jenkins/jenkins:latest')
+
+        with open('ips.txt', 'w') as f:
+            ip = subprocess.check_output(f"docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' {name}", shell=True, text=True).strip()
+            f.write(f'{name}:{ip}')
+            
     else:
         print('Opção não encontrada.')
 
