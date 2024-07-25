c3de7aa21b7a4b48b7793135cfdb545c94986f2e
dg-delbank <douglas.santos@delbank.com.br>
2024-07-25 11:39:27 -0300

    Changes

--- app.py
+++ app.py
@@ -12,50 +12,48 @@ try:
 except Exception as ex:
     print('Opção inválida.' + ex)
 
-def verify_choice(choice):
-    if '1' in choice:
-        name = input('Digite o nome do container do Portainer: ') or 'portainer-delbank-hml-master'
-        os.system(f'docker volume create portainer_data')
-        os.system(f'docker run -d -p 8000:8000 -p 9443:9443 --name {name} --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest')
+with open('ips.txt', 'w') as f:
+    def verify_choice(choice):
+        if '1' in choice:
+            name = input('Digite o nome do container do Portainer: ') or 'portainer-delbank-hml-master'
+            os.system(f'docker volume create portainer_data')
+            os.system(f'docker run -d -p 8000:8000 -p 9443:9443 --name {name} --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest')
 
-        with open('ips.txt', 'w') as f:
             ip = subprocess.check_output(f"docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' {name}", shell=True, text=True).strip()
-            f.write(f'{name}:{ip}')
+            f.write(f'{name}:{ip}\n')
 
-    if '2' in choice:
-        name = input('Digite o nome do container do Nginx: ') or 'nginx-proxy-manager-delbank-hml-master'
-        with open('docker-compose.yml', 'w') as f:
-            f.write(f'''version: '3.8'
-services:
-  app:
-    container_name: {name}
-    image: 'jc21/nginx-proxy-manager:latest'
-    restart: unless-stopped
-    ports:
-      - '80:80'
-      - '81:81'
-      - '443:443'
-    volumes:
-      - ./data:/data
-      - ./letsencrypt:/etc/letsencrypt
-''')
-        os.system('docker compose up -d')
+        if '2' in choice:
+            name = input('Digite o nome do container do Nginx: ') or 'nginx-proxy-manager-delbank-hml-master'
+            with open('docker-compose.yml', 'w') as docker_compose_file:
+                docker_compose_file.write(f'''version: '3.8'
+    services:
+      app:
+        container_name: {name}
+        image: 'jc21/nginx-proxy-manager:latest'
+        restart: unless-stopped
+        ports:
+          - '80:80'
+          - '81:81'
+          - '443:443'
+        volumes:
+          - ./data:/data
+          - ./letsencrypt:/etc/letsencrypt
+    ''')
+            os.system('docker compose up -d')
 
-        with open('ips.txt', 'w') as f:
             ip = subprocess.check_output(f"docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' {name}", shell=True, text=True).strip()
-            f.write(f'{name}:{ip}')
+            f.write(f'{name}:{ip}\n')
 
-    if '3' in choice:
-        name = input('Digite o nome do container do Jenkins: ') or 'jenkins-delbank-hml-master'
-        os.system(f'docker run -d -it --restart always -u root -p 8080:8080 -p 50000:50000 -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker --name {name} jenkins/jenkins:latest')
+        if '3' in choice:
+            name = input('Digite o nome do container do Jenkins: ') or 'jenkins-delbank-hml-master'
+            os.system(f'docker run -d -it --restart always -u root -p 8080:8080 -p 50000:50000 -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker --name {name} jenkins/jenkins:latest')
 
-        with open('ips.txt', 'w') as f:
             ip = subprocess.check_output(f"docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' {name}", shell=True, text=True).strip()
-            f.write(f'{name}:{ip}')
-            
-    else:
-        print('Opção não encontrada.')
+            f.write(f'{name}:{ip}\n')
 
-verify_choice(choice)
+        else:
+            print('Opção não encontrada.')
+
+    verify_choice(choice)
 
 print('Fim do seja lá o que acabou de acontecer.')
