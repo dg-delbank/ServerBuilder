cef649f52fb086f721ccce0001ff20fc067a9dbd
dg-delbank <douglas.santos@delbank.com.br>
2024-07-25 11:48:53 -0300

    Changes

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ with open('ips.txt', 'w') as f:
             os.system(f'docker volume create portainer_data')
             os.system(f'docker run -d -p 8000:8000 -p 9443:9443 --name {name} --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest')
 
-            ip = subprocess.check_output(f"docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' {name}", shell=True, text=True).strip()
+            ip = subprocess.check_output(f"docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' {name}", shell=True).strip()
             f.write(f'{name}:{ip}\n')
 
         if '2' in choice:
@@ -41,14 +41,14 @@ with open('ips.txt', 'w') as f:
     ''')
             os.system('docker compose up -d')
 
-            ip = subprocess.check_output(f"docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' {name}", shell=True, text=True).strip()
+            ip = subprocess.check_output(f"docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' {name}", shell=True).strip()
             f.write(f'{name}:{ip}\n')
 
         if '3' in choice:
             name = input('Digite o nome do container do Jenkins: ') or 'jenkins-delbank-hml-master'
             os.system(f'docker run -d -it --restart always -u root -p 8080:8080 -p 50000:50000 -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker --name {name} jenkins/jenkins:latest')
 
-            ip = subprocess.check_output(f"docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' {name}", shell=True, text=True).strip()
+            ip = subprocess.check_output(f"docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' {name}", shell=True).strip()
             f.write(f'{name}:{ip}\n')
 
         else:
