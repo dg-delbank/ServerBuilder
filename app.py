6e6855565a947c0aa06a2b7e1c2853890dcdafe6
dg-delbank <douglas.santos@delbank.com.br>
2024-08-28 16:02:14 -0300

    Changes

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ volumes:
 
         if '3' in choice:
             os.system('docker volume create jenkins_data')
-            os.system(f'docker run -d --restart always -u 1000:1000 -p 8080:8080 -p 50000:50000 -v /var/run/docker.sock:/var/run/docker.sock -v jenkins_data:/var/jenkins_home --name {nameJenkins} jenkins/jenkins:latest')
+            os.system(f'docker run -d --restart always -p 8080:8080 -p 50000:50000 -v /var/run/docker.sock:/var/run/docker.sock -v jenkins_data:/var/jenkins_home --name {nameJenkins} jenkins/jenkins:latest')
 
             comando = ['docker', 'inspect', '-f', '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}', nameJenkins]
             ip = subprocess.check_output(comando, stderr=subprocess.STDOUT, text=True)
